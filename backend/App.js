5c3f8309751631d8629d69ae470911fb666328c8
Harshith Renikindi <harshith.renikidi@kalvium.community>
2025-01-13 18:01:11 +0530

    m4 done

--- backend/App.js
+++ backend/App.js
@@ -1,15 +1,24 @@
 const express=require('express')
 const connectDatabase=require('./db/Database');
 const ErrorHandler = require('./utils/ErrorHandler');
+const cookieParser=require('cookie-parser');
+const bodyParser=require('body-parser');
 const app=express()
 
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"))
+app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}))
 //config
 if(process.env.NODE_ENV !=="PRODUCTION"){
     require('dotenv').config({
         path:'backend/config/.env'
     })
 }
+//import router
+const user=require('./controller/user')
 
+app.use("/api/v2/user",user)
 connectDatabase();
 
 app.use(ErrorHandler);
