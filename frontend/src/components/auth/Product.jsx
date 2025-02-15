1d4e09dc440cd26c7a1c56d5f3bed05a01476cfc
Harshith Renikindi <harshith.renikidi@kalvium.community>
2025-02-15 10:06:29 +0530

    M16 done

--- frontend/src/components/auth/Product.jsx
+++ frontend/src/components/auth/Product.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 import {React,useState,useEffect} from 'react';
-
-const Product = ({ name, images, description, price }) => {
+import { useNavigate } from "react-router-dom";
+const Product = ({ _id,name, images, description, price }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
   useEffect(() => {
     if (!images || images.length === 0) return;
     const interval = setInterval(() => {
@@ -27,7 +28,9 @@ const Product = ({ name, images, description, price }) => {
       </div>
       <div className="w-full text-center">
         <p className="text-lg font-bold text-gray-900 mb-4">${price.toFixed(2)}</p>
-        <button className="text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded-full transition-all">
+        <button className="text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded-full transition-all" 
+         onClick={() => navigate(`/product/${_id}`)}
+        >
           More Info
         </button>
       </div>
@@ -36,8 +39,9 @@ const Product = ({ name, images, description, price }) => {
 };
 
 Product.propTypes = {
+  _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  images: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
   description: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
 };
