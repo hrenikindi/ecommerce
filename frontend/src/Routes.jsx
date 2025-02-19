6be1ea1c3c796f6dfed4095ea38b02ff64457b11
hrenikindi <159125359+hrenikindi@users.noreply.github.com>
2025-02-19 11:54:31 +0530

    Delete frontend directory

--- frontend/src/Routes.jsx
+++
@@ -1,8 +0,0 @@
-import LoginPage from './pages/Login'
-import SignupPage from './pages/Signup'
-import Home from './pages/Home'
-import CreateProduct from './pages/createProduct';
-import MyProducts from './pages/myProduct';
-import Cart from './pages/cart';
-import ProductDetails from './pages/productDetails';
-export {LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart,ProductDetails};
